76628c6df918132021e28b7e815899202759c08e
Kripasnair2000 <kripa.s@dridatah.com>
2024-08-23 11:29:16 +0530

    My Maven Project

---
+++ Mymavenproject/src/main/java/mavenproject/Switch_Program.java
@@ -0,0 +1,23 @@
+package mavenproject;
+
+public class Switch_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int day=1;
+		switch(day)
+		{
+		case 1 :System.out.println("ONE");
+		        break;
+		case 2 :System.out.println("TWO");
+		        break;
+		case 3 :System.out.println("THREE");
+		        break;
+		case 4 :System.out.println("FOUR");
+		        break;
+		default:System.out.println("INVALID");
+		}
+
+	}
+
+}
